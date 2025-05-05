7e2829287c1a8bdcfdc34dbf74ffcae9581624a1
YASHJADON5 <jadonyash2@gmail.com>
2025-05-05 15:38:23 +0530

    get classes controller and validator fixed

--- server/validator/admin/class.validator.js
+++ server/validator/admin/class.validator.js
@@ -19,7 +19,9 @@ export const createClassBodySchema = z.object({
 
 export const getClassesQuerySchema = z.object({
     limit:z.coerce.number().default(10),
-    page:z.coerce.number().default(1)
+    page:z.coerce.number().default(1),
+    date:z.coerce.date().optional(),
+    location:z.string().trim().optional()
 })
 
 export const deleteClassParamsSchema = z.object({
