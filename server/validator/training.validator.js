3a0e5a9deb3b37a6367c56e733c6386f156c081f
YASHJADON5 <jadonyash2@gmail.com>
2025-05-07 12:55:28 +0530

    training validator fixed

--- server/validator/training.validator.js
+++ server/validator/training.validator.js
@@ -9,8 +9,8 @@ export const createTrainingBodySchema = z.object({
     price:z.coerce.number(), 
     description:z.string().trim(),
     moreInfo:z.array(z.object({
-        title:z.string().trim(),
-        description:z.string().trim(),
+        title:z.string().trim().optional(),
+        description:z.string().trim().optional(),
     })).optional(),
     category: z.enum(['teacherTraining', 'youMayAlsoLike']),
     available: z.string().transform(val => val === "true"),
