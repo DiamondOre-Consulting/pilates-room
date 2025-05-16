097eb8b613a0b6a2458509a711c1f075c5e10af3
DiamondOreDev <harsh.diamondore@gmail.com>
2025-05-16 17:55:06 +0530

    helper changed

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // https://pilates-room-backend.onrender.com
-const BASE_URL = 'http://localhost:8000/api/v1' 
-// const BASE_URL = 'https://pilates-room-backend.onrender.com'
+// const BASE_URL = 'http://localhost:8000/api/v1' 
+const BASE_URL = 'https://pilates-room-backend.onrender.com'    
 // http://localhost:8000/api/v1
 const userAxiosInstance =  axios.create();
 userAxiosInstance.defaults.baseURL =BASE_URL;
