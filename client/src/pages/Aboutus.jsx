d1a7270a00165b0404d3fa7c7582583142de482e
Akash Singh <akashsingh@diamondore.in>
2025-05-31 16:06:59 +0530

    Config checked

--- client/src/pages/Aboutus.jsx
+++ client/src/pages/Aboutus.jsx
@@ -2,10 +2,8 @@ import AboutSection from '@/components/AboutUsComponents/AboutSection'
 import OurTeam from '@/components/AboutUsComponents/OurTeam'
 import React, { useEffect } from 'react'
 
-
 const Aboutus = () => {
 
-  
       useEffect(() => {
         window.scrollTo(0, 0);
       }, []);
