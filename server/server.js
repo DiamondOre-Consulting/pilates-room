7e9a589cffefd644006f6514c9e4f4e3bdab2050
Zoya774 <zoya.rasonline@gmail.com>
2025-08-14 15:24:57 +0530

    update changes in server and env

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 import app from "./app.js";
 import connectToDb from "./config/db.config.js";
 
+const port = process.env.PORT || 5000;
 
-const port =  8000;
-
-
-
-app.listen(port, async() => {
-    await connectToDb();
-    console.log(`Server is running on port ${port}`);
+app.listen(port, async () => {
+  await connectToDb();
+  console.log(`Server is running on port ${port}`);
 });
